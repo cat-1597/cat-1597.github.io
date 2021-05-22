ab0be0a5567c70b6ccd523ffc38c7e42dc1f437a
Cat Chiong <cchiong1597@gmail.com>
2021-05-22 15:11:06 -0400

    Converted to JSX

--- TH_scoreboard/scoreboard/app.js
+++ TH_scoreboard/scoreboard/app.js
@@ -2,26 +2,33 @@
 //Title is an object
 
 //React creates plain js scripts - <h1 id="main-title title='This is a title">My First React Element!</h1>
-const title = React.createElement( 
+/*const title = React.createElement( 
     'h1',
     { id: 'main-title', title: 'This is a title.'},
     //children of the element
     'My First React Element!'
-);
-//Create other elements
+);*/
 
-const description = React.createElement(
+//To convert to JSX
+
+const title = <h1>My First React Element</h1>
+
+//Create other elements
+/*const description = React.createElement(
     'p',
     null,
     'My first paragraph!'
-);
+); */
+
+//To convert to JSX
+const description = <p>I just learned how to create a React node and render it into the DOM.</p>;
+
 
 //Combines both Title and Description elements 
 const header = React.createElement(
     'header',
     null,
     title, description
-
 );
 //creating and updating DOM - ReactDOM.render function - connects react to DOM
 //accepts two arguments and 1) react element or javascript object describes element, 2) actual html document.getElementID is the container
