40d2ba56e797e37ee10087029564f55e6be6306a
Cat Chiong <cchiong1597@gmail.com>
2021-05-22 15:30:20 -0400

    Convert to JSX

--- TH_scoreboard/scoreboard/app.js
+++ TH_scoreboard/scoreboard/app.js
@@ -11,7 +11,7 @@
 
 //To convert to JSX
 
-const title = <h1>My First React Element</h1>
+//const title = <h1>My First React Element</h1>
 
 //Create other elements
 /*const description = React.createElement(
@@ -21,18 +21,40 @@ const title = <h1>My First React Element</h1>
 ); */
 
 //To convert to JSX
-const description = <p>I just learned how to create a React node and render it into the DOM.</p>;
+//const description = <p>I just learned how to create a React node and render it into the DOM.</p>;
 
 
 //Combines both Title and Description elements 
-const header = React.createElement(
+/*const header = React.createElement(
     'header',
     null,
     title, description
+);*/
+
+//To convert parent element to children
+/*const header = (
+<header>
+    <h1>My First React Element</h1>
+    <p>I just learned how to create a React node and render it into the DOM.</p>;
+</header>
 );
 //creating and updating DOM - ReactDOM.render function - connects react to DOM
 //accepts two arguments and 1) react element or javascript object describes element, 2) actual html document.getElementID is the container
+*/
 
+//To convert to javascript expressions into strings
+
+//const title = 'My First React Element';
+const description = 'I just learned how to create a node and render it into the DOM.';
+const myTitleID = 'main-title';
+const name = 'Kia';
+
+const header = (
+    <header>
+        <h1 id={myTitleID}>{ name }'s First React Element</h1>
+            <p>{ description }</p>
+    </header>
+);
 
 ReactDOM.render(
     header, 
